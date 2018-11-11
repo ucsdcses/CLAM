bcac6004b1d6a6cb974a63a9c18db26651f1a6d8
Nlin2 <nil021@ucsd.edu>
2018-11-10 18:35:31 -0800

    updated wToCSV.py

--- w/wToCSV.py
+++ w/wToCSV.py
@@ -1,7 +1,7 @@
 # Potentially useful if we encounter large files.
-#Not sure if we will need other info
+# Not sure if we will need other info
 with open('users.txt', 'r') as users:
     with open('w.csv', 'w') as w:
         for line in users:
             w.write(','.join(line.split()) + '\n')
-
+        
